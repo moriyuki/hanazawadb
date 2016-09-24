56cef8009c0fa57bc26ed0b114e548761384a475
alexfujimura <y.fujimura@do-will.org>
2016-09-24 12:52:50 +0900

    SoftwareMaster,UserMasterのフォームの追加

--- Sudachipon/Sudachipon/DbAccessor.cs
+++ Sudachipon/Sudachipon/DbAccessor.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 using Npgsql;
 
 namespace Sudachipon
@@ -28,6 +29,8 @@ namespace Sudachipon
         // SELECT Execute
         NpgsqlDataReader exeuteSql(String sql)
         {
+            // NpgsqlDataReader ret = new NpgsqlDataReader();
+
             if (string.IsNullOrEmpty(sql))
             {
                 return null;
@@ -38,10 +41,11 @@ namespace Sudachipon
                 conn.Open();
 
                 var command = new NpgsqlCommand(sql, conn);
+                // System.Windows.Forms.MessageBox.Show("record number",String.Format("{0}", (int)command.ExecuteScalar()));
                 var dataReader = command.ExecuteReader();
+
                 return dataReader;
             }
-
         }
 
         // INSERT UPDATE Execute
@@ -54,8 +58,98 @@ namespace Sudachipon
             }
         }
         // ========================= DB access =========================
+        public
+        void SelectPcMaster()
+        {
+            String sql = "select * from mt_pc";
 
+            using (var conn = new NpgsqlConnection(CONN_STRING))
+            {
+                conn.Open();
 
+                var command = new NpgsqlCommand(sql, conn);
+                // System.Windows.Forms.MessageBox.Show("record number",String.Format("{0}", (int)command.ExecuteScalar()));
+                var dataReader = command.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    PcMaster pm = new PcMaster();
+                    pm.createInitialData();
+                    
+                    pm.Id = int.Parse(String.Format("{0}",dataReader["pc_id"]));
+                    pm.Name = String.Format("{0}", dataReader["pc_name"]);
+                    pm.Os = String.Format("{0}", dataReader["pc_os"]);
+                    pm.Memory = String.Format("{0}", dataReader["pc_memory"]);
+                    pm.Cpu = String.Format("{0}", dataReader["pc_cpu"]);
+                    pm.Active = bool.Parse(String.Format("{0}", dataReader["pc_active"]));
+                    pm.IsByod = bool.Parse(String.Format("{0}", dataReader["pc_is_byod"]));
+                    pm.Comment = String.Format("{0}", dataReader["pc_comment"]);
+
+                    this.PcMasters.Add(pm);
+                   // System.Windows.Forms.MessageBox.Show(String.Format("{0}", dataReader[0]));
+                }
+            }
+        }
+
+        void SelectSoftwareMaster()
+        {
+            String sql = "select * from mt_software";
+
+            using (var conn = new NpgsqlConnection(CONN_STRING))
+            {
+                conn.Open();
+
+                var command = new NpgsqlCommand(sql, conn);
+                // System.Windows.Forms.MessageBox.Show("record number",String.Format("{0}", (int)command.ExecuteScalar()));
+                var dataReader = command.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    SoftwareMaster sm = new SoftwareMaster();
+                    sm.createData();
+
+                    sm.id = int.Parse(String.Format("{0}", dataReader["sf_id"]));
+                    sm.name = String.Format("{0}", dataReader["sf_name"]);
+                    sm.version = String.Format("{0}", dataReader["sf_version"]);
+                    sm.osType = int.Parse(String.Format("{0}", dataReader["sf_os"]));
+                    sm.available = int.Parse(String.Format("{0}", dataReader["sf_available"]));
+                    sm.active = bool.Parse(String.Format("{0}", dataReader["sf_active"]));
+                    sm.comment = String.Format("{0}", dataReader["sf_comment"]);
+
+                    this.SoftwareMasters.Add(sm);
+                    // System.Windows.Forms.MessageBox.Show(String.Format("{0}", dataReader[0]));
+                }
+            }
+        }
+
+        void SelectUserMaster()
+        {
+            String sql = "select * from mt_user";
+
+            using (var conn = new NpgsqlConnection(CONN_STRING))
+            {
+                conn.Open();
+
+                var command = new NpgsqlCommand(sql, conn);
+                // System.Windows.Forms.MessageBox.Show("record number",String.Format("{0}", (int)command.ExecuteScalar()));
+                var dataReader = command.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    UserMaster um = new UserMaster();
+                    um.createData();
+
+                    um.id = int.Parse(String.Format("{0}", dataReader["us_id"]));
+                    um.name = String.Format("{0}", dataReader["us_name"]);
+                    um.type = int.Parse(String.Format("{0}", dataReader["us_type"]));
+                    um.active = bool.Parse(String.Format("{0}", dataReader["us_active"]));
+                    um.comment = String.Format("{0}", dataReader["us_comment"]);
+
+                    this.UserMasters.Add(um);
+                    // System.Windows.Forms.MessageBox.Show(String.Format("{0}", dataReader[0]));
+                }
+            }
+        }
         // ========================= data model =========================
         public
         // PC_master
@@ -73,7 +167,7 @@ namespace Sudachipon
             String _comment;
 
             public
-            PcMaster createData()
+            PcMaster createInitialData()
             {
                 PcMaster pm = new PcMaster();
                 pm._id = 0;
@@ -88,11 +182,15 @@ namespace Sudachipon
                 return pm;
             }
 
+            public override string ToString()
+            {
+                return _name;
+            }
             // pc_id の最大値+1を返す
-            int GetNextId()
+            public int GetNextId()
             {
                 int ret = 0;
-                foreach (var pc in PcMasters)
+                foreach (PcMaster pc in _DbAccessor.PcMasters)
                 {
                     if (ret < pc.Id) ret = pc.Id;
                 }
@@ -100,7 +198,7 @@ namespace Sudachipon
             }
 
             // idプロパティ
-            int Id
+            public int Id
             {
                 get
                 {
@@ -116,7 +214,7 @@ namespace Sudachipon
             }
 
             // nameプロパティ
-            String Name
+            public String Name
             {
                 get
                 {
@@ -129,7 +227,7 @@ namespace Sudachipon
             }
 
             // osプロパティ
-            String Os
+            public String Os
             {
                 get
                 {
@@ -142,7 +240,7 @@ namespace Sudachipon
             }
 
             // memoryプロパティ
-            String Memory
+            public String Memory
             {
                 get
                 {
@@ -155,7 +253,7 @@ namespace Sudachipon
             }
 
             // cpuプロパティ
-            String Cpu
+            public String Cpu
             {
                 get
                 {
@@ -166,48 +264,102 @@ namespace Sudachipon
                     _cpu = value;
                 }
             }
+
+            // active 
+            public bool Active
+            {
+                get
+                {
+                    return _active;
+                }
+                set
+                {
+                    _active = value;
+                }
+            }
+
+            // byod
+            public bool IsByod
+            {
+                get
+                {
+                    return _isByod;
+                }
+                set
+                {
+                    _isByod = value;
+                }
+            }
+
+            // comment
+            public String Comment
+            {
+                get
+                {
+                    return _comment;
+                }
+                set
+                {
+                    _comment = value;
+                }
+            }
+
         }
 
-        static PcMaster[] PcMasters;
+        public List<PcMaster> PcMasters = new List<PcMaster>();
 
         // Soft master
+        public
         struct SoftwareMaster
         {
             // SoftwareataModel;
+            public
             int id;
+            public
             String name;
+            public
             String version;
+            public
             int osType;
-            int avalable;
+            public
+            int available;
+            public
             bool active;
+            public
             String comment;
 
-            SoftwareMaster createData()
+            public SoftwareMaster createData()
             {
                 SoftwareMaster sm = new SoftwareMaster();
                 sm.id = 0;
                 sm.name = String.Empty;
                 sm.version = String.Empty;
                 sm.osType = 1;
-                sm.avalable = 1;
+                sm.available = 1;
                 sm.active = true;
                 sm.comment = String.Empty;
                 return sm;
             }
         }
 
-        SoftwareMaster[] SoftwareMasters;
+        public List<SoftwareMaster> SoftwareMasters = new List<SoftwareMaster>();
 
         // User master
+        public
         struct UserMaster
         {
+            public
             int id;
+            public
             String name;
+            public
             int type;
+            public
             bool active;
+            public
             String comment;
 
-            UserMaster createData()
+            public UserMaster createData()
             {
                 UserMaster um = new UserMaster();
                 um.id = 0;
@@ -220,7 +372,7 @@ namespace Sudachipon
             }
         }
 
-        UserMaster[] UserMasters;
+        public List<UserMaster> UserMasters = new List<UserMaster>();
 
         // Pc Soft Relation Data
         struct PcSoftData
--- Sudachipon/Sudachipon/FrmPcMasterMaintenance.cs
+++ Sudachipon/Sudachipon/FrmPcMasterMaintenance.cs
@@ -12,9 +12,39 @@ namespace Sudachipon
 {
     public partial class FrmPcMasterMaintenance : Form
     {
+        DbAccessor dba = DbAccessor.GetInstance();
+        DbAccessor.PcMaster selectedPc;
+
         public FrmPcMasterMaintenance()
         {
             InitializeComponent();
+            dba.SelectPcMaster();
+
+            
+            foreach (DbAccessor.PcMaster pc in dba.PcMasters)
+            {
+                this.lbxPcs.Items.Add(pc);
+            }
+        }
+
+        // 選択変更時
+        private void lbxPcs_SelectedValueChanged(object sender, EventArgs e)
+        {
+            //if (selectedPc) {
+            //    return;
+            //}
+
+            // selectedPc = (DbAccessor.PcMaster)sender;
+
+            // 詳細項目クリア
+            // this.txbPcName.Text = selectedPc.Name;
+
+            // 詳細項目値代入
+        }
+
+        private void lbxPcs_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
